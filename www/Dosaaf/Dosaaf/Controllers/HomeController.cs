9ccb7d985acf87a007837481789aad4b9e2b0b57
zmqp <domarlaid2@mail.ru>
2022-08-19 23:33:51 +0300

    Added online education page and record page
    [+] Add OnlineEducation page with form
    [+] Add RecordEducation page with form
    [+] Add button functional
    [+] Add adaptation for phone, landscape

--- www/Dosaaf/Dosaaf/Controllers/HomeController.cs
+++ www/Dosaaf/Dosaaf/Controllers/HomeController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Dosaaf.Controllers
 {
     public class HomeController : Controller
     {
+        public record Category(int Id, string Name);
+        IEnumerable<Category> categories = new List<Category> {
+            new Category (1, "Категория А"),
+            new Category (2, "Категория В"),
+            new Category (3, "Категория С (c 'В' на 'C')"),
+            new Category (4, "Категория С (c 'С' на 'В')"),
+            new Category (5, "Категория С (c 'В' на 'Д')"),
+            new Category (6, "Категория CE"),
+            new Category (7, "Теоретический минимум")
+            
+        };
         public IActionResult Index()
         {
             return View();
@@ -18,6 +30,16 @@ namespace Dosaaf.Controllers
         }
         public IActionResult Contacts()
         {
+            return View();
+        }
+        public IActionResult OnlineEducation()
+        {
+            ViewBag.Categories = new SelectList(categories, "Name", "Name");
+            return View();
+        }
+        public IActionResult RecordEducation()
+        {
+            ViewBag.Categories = new SelectList(categories, "Name", "Name");
             return View();
         }
     }
---
+++ www/Dosaaf/Dosaaf/Models/OnlineEducationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Dosaaf.Models
+{
+    public class OnlineEducationModel
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Category { get; set; }
+    }
+}
